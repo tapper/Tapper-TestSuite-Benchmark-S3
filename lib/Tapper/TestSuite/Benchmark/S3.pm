9bc5f173176cf9da6dbeb5ea5d60049525026f0a
Maik Hentsche <maik.hentsche@amd.com>
2012-06-08 10:33:02 +0200

    initial testsuite for suspend/resume measurements

---
+++ lib/Tapper/TestSuite/Benchmark/S3.pm
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+package Tapper::TestSuite::Benchmark::S3;
+# ABSTRACT: Measure suspend/resume cycle with external Arduino box
+
+1;
+
+__END__
+
+=head1 ABOUT
+
+This testsuite triggers suspend resume cycle with an external Arduino
+box and reports values into Tapper suiting automatic passthrough to
+Codespeed graph rendering application.
+
+=cut
